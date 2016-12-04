ddf00a9834a55679a9bb1453fbdaeb43920311f9
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2016-12-04 16:11:24 +0200

    E1.29 - calculating integrals with Simpson's rule
    Running this with (simpsons-integral cube 0 1 n) with n being any even
    number, it currently returns 1/4 - an exact rational number. This is
    unexpected, because this was supposed to be an approximation. Changing
    the 3 to be 3.0 and wrapping n in (exact->inexact n) in the division for
    h changes the result to be a float and look more like an approximation,
    but I'm not sure if that is to be desired here. Doing these calculations
    by hand for n=2 shows that the result can be found using just rational
    numbers and comes out to be 1/4, so maybe it's best to let Scheme do
    it's thing and not force it to use floats.

---
+++ exercise-1.29.scm
@@ -0,0 +1,17 @@
+(define (sum term a next b)
+  (if (> a b)
+    0
+    (+ (term a)
+       (sum term (next a) next b))))
+
+(define (simpsons-integral f a b n)
+  (define h (/ (- b a) n))
+  (define (y k) (f (+ a (* k h))))
+  (define (simpsons-term k)
+    (cond ((or (= k 0) (= k n)) (y k))
+          ((even? k) (* 2 (y k)))
+          (else (* 4 (y k)))
+          ))
+  (define (simpsons-next k) (+ 1 k))
+  (/ (* h (sum simpsons-term 0 simpsons-next n)) 3)
+  )
