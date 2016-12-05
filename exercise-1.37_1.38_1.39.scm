8e63e12bf133d3c6de7432e6e09050088fab9d4e
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2016-12-06 00:06:52 +0200

    E1.38,1.39 - Approximating e and tan
    The tan function kept giving me weird results for the longest time and I
    couldn't figure out what's amiss. Turned out I'd gotten the n and d
    functions mixed up. It's probably too late for doing this.
    
    This is the output of the test run I used for the tan approximation:
    ```
    > (approximate-to-precision (lambda (k) (tan-cf-approx 2.0 k)) 0.00001)
    '(8 -2.185039871780394)
    ```

--- exercise-1.37_1.38_1.39.scm
+++ exercise-1.37_1.38_1.39.scm
@@ -1,3 +1,4 @@
+; 1.37a
 (define (rec-cont-frac n d k)
   (define (iter i)
     (if (>= i k)
@@ -17,6 +18,7 @@
         (try current-value (inc step)))))
   (try (f 1) 2))
 
+; 1.37b
 (define (iter-cont-frac n d k)
   (define (iter i acc)
     (if (<= i 0)
@@ -25,3 +27,20 @@
   (iter k 0))
 (define (iter-golden-ratio-approx k)
   (/ 1 (iter-cont-frac (always 1.0) (always 1.0) k)))
+
+; 1.38
+(define (e-approx k)
+  (define (d i)
+    (if (= (modulo i 3) 2)
+      (* (+ 1 (quotient i 3)) 2)
+      1))
+  (+ 2 (iter-cont-frac (always 1.0) d k)))
+
+; 1.39
+(define (tan-cf-approx x k)
+  (define (n i)
+    (if (= i 1)
+      x
+      (- (square x))))
+  (define (d i) (- (* i 2) 1))
+  (iter-cont-frac n d k))
