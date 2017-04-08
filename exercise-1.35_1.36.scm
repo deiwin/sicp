35802b22171a130dded4286dda8716e9ff214e87
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-04-08 18:40:22 +0300

    E1.40 - Approximating zeroes of a cubic function

--- exercise-1.35_1.36.scm
+++ exercise-1.35_1.36.scm
@@ -1,14 +1,3 @@
-(define tolerance 0.00001)
-(define (fixed-point f first-guess)
-  (define (close-enough? v1 v2) (< (abs (- v1 v2)) tolerance))
-  (define (try guess n)
-    (printf "guess ~a: ~a\n" n guess)
-    (let (( next (f guess)))
-      (if (close-enough? guess next)
-        next
-        (try next (+ n 1)))))
-  (try first-guess 1))
-
 (define (golden-ratio-f x) (+ 1 (/ 1 x)))
 (define (xx-f x) (/ (log 1000) (log x)))
 (define (damp f)
--- lib.scm
+++ lib.scm
@@ -17,3 +17,13 @@
     a
     (gcd b (remainder a b))))
 
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2) (< (abs (- v1 v2)) tolerance))
+  (define (try guess n)
+    (printf "guess ~a: ~a\n" n guess)
+    (let (( next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next (+ n 1)))))
+  (try first-guess 1))
