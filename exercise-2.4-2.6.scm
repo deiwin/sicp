691b8830e98f9f1fd07ca669d0cd28a1b17d1c68
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-07-22 12:05:48 +0300

    E2.4-2.6 - Representing data through procedures
    I needed some sort of a function to confirm whether the numbers I was
    creating were correct. Initially created a function that printed as many
    dots onto the screen as was the number's value. Then realized I could
    actually print the real number, by changing the dot-printing function to
    just increment the value provided to it and seeding it with 0. Then
    realized that there's no need to do the printing, when I can just find
    the number and have the procedure (now named `eval`) return the value.
    
    I don't _really_ understand why my implementation of `plus` works, but I
    had a hunch and tried and it does seem to work. I have the beginnings of
    an understanding but can't claim to actually fully understand.

---
+++ exercise-2.4-2.6.scm
@@ -0,0 +1,53 @@
+; 2.4
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+; 2.5
+(define (cons x y)
+  (*
+    (expt 2 x)
+    (expt 3 y)))
+
+(define (car z)
+  (define (twos z)
+    (if (= (remainder z 3) 0)
+      (twos (/ z 3))
+      z))
+  (log-with-base 2 (twos z)))
+
+(define (cdr z)
+  (define (threes z)
+    (if (= (remainder z 2) 0)
+      (threes (/ z 2))
+      z))
+  (log-with-base 3 (threes z)))
+
+; 2.6
+(define zero
+  (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(define one
+  (lambda (f) (lambda (x) (f x))))
+
+(define two
+  (lambda (f) (lambda (x) (f (f x)))))
+
+(define three
+  (lambda (f) (lambda (x) (f (f (f x))))))
+
+(define five
+  (lambda (f) (lambda (x) (f (f (f (f (f x))))))))
+
+(define (plus a b)
+  (lambda (f) (lambda (x) ((a f) ((b f) x)))))
+
+(define (eval n) ((n inc) 0))
