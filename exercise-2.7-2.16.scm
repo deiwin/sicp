b0348d765586d39e63bb89f9b0081c8af82b9d1a
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-07-22 13:00:30 +0300

    E2.9 - Width-based interval arithmetic

--- exercise-2.7-2.16.scm
+++ exercise-2.7-2.16.scm
@@ -26,3 +26,35 @@
   (mul-interval x
                 (make-interval (/ 1.0 (upper-bound y))
                                (/ 1.0 (lower-bound y)))))
+
+(define (midpoint-interval i)
+  (average (lower-bound i) (upper-bound i)))
+(define (width-interval i)
+  (/
+    (- (upper-bound i) (lower-bound i))
+    2))
+
+(define (alt-add-interval x y)
+  (let ((midpoint (+ (midpoint-interval x) (midpoint-interval y)))
+        (width (+ (width-interval x) (width-interval y))))
+    (make-interval (- midpoint width)
+                   (+ midpoint width))))
+
+(define (alt-sub-interval x y)
+  (let ((midpoint (- (midpoint-interval x) (midpoint-interval y)))
+        (width (+ (width-interval x) (width-interval y))))
+    (make-interval (- midpoint width)
+                   (+ midpoint width))))
+
+(define i1 (make-interval -1 1))
+(define i2 (make-interval 1 3))
+
+; (= (width-interval i1) (width-interval i2))
+; The above is true, so if the width of the multiplied interval only depended
+; on the widths of the multiplied intervals then the following would also have
+; to be true:
+; (= (width-interval (mul-interval i1 i2))
+;    (width-interval (mul-interval i2 i2)))
+; But that's not the case. Therefore the width of the multiplied interval
+; cannot only depend on the widths of the multiplied intervals. Same can be
+; shown for division in the same manner.
