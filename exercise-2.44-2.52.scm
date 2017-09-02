e3ab540f2d015b85721073ca044bdd2d2a2ae17c
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-09-02 17:34:26 +0300

    Paint painting into the console
    Actually figured out a way to do this reasonably well. For painting
    `(paint (up-split painter 3))` it creates the following output, for
    example:
    
    ```
    XXXXXXXXXXXXXXXX
    XXXXXXXXXXXXXXXX
    X--XX--XX--XX--X
    X--XX--XX--XX--X
    X------XX------X
    |      ||      |
    |      ||      |
    X------XX------X
    X--------------X
    |              |
    |              |
    |              |
    |              |
    |              |
    |              |
    X--------------X
    ```

--- exercise-2.44-2.52.scm
+++ exercise-2.44-2.52.scm
@@ -15,6 +15,54 @@
 (define right-painter caddr)
 (define left-painter cadr)
 
+(define make-scale cons)
+(define scale-x car)
+(define scale-y cdr)
+
+(define (min-scale painter)
+  (cond ((atomic-painter? painter) (make-scale 2 2))
+        ((below? painter) (let ((min-top-scale (min-scale (top-painter painter)))
+                                (min-bot-scale (min-scale (bottom-painter painter))))
+                            (make-scale (max (scale-x min-top-scale) (scale-x min-bot-scale))
+                                        (* 2 (max (scale-y min-top-scale) (scale-y min-bot-scale))))))
+        ((beside? painter) (let ((min-left-scale (min-scale (left-painter painter)))
+                                 (min-right-scale (min-scale (right-painter painter))))
+                             (make-scale (* 2 (max (scale-x min-left-scale) (scale-x min-right-scale)))
+                                         (max (scale-y min-left-scale) (scale-y min-right-scale)))))))
+
+(define horizontal-border "-")
+(define vertical-border "|")
+(define empty-point " ")
+(define corner "X")
+
+; A painting is represented as a 2 dimensional array, where X characters
+; signify a corner of a sub-painting.
+(define (paint painter)
+  (define (iter painter scale)
+    (cond ((atomic-painter? painter)
+           (let ((terminal-row (cons corner
+                                     (append (repeat horizontal-border (- (scale-x scale) 2)) (list corner))))
+                 (inner-row (cons vertical-border
+                                  (append (repeat empty-point (- (scale-x scale) 2)) (list vertical-border)))))
+             (cons terminal-row (append (repeat inner-row (- (scale-y scale) 2)) (list terminal-row)))))
+          ((below? painter)
+           (let ((split-scale (make-scale (scale-x scale)
+                                          (/ (scale-y scale) 2))))
+             (append (iter (top-painter painter) split-scale)
+                     (iter (bottom-painter painter) split-scale))))
+          ((beside? painter)
+           (let ((split-scale (make-scale (/ (scale-x scale) 2)
+                                          (scale-y scale))))
+             (let ((left-painting (iter (left-painter painter) split-scale))
+                   (right-painting (iter (right-painter painter) split-scale)))
+               (map append left-painting right-painting))))))
+  (iter painter (min-scale painter)))
+
+(define (serialize-painting painting)
+  (string-join (map (lambda (row) (string-join row ""))
+                    painting)
+               "\n"))
+
 ; 2.44
 (define (up-split painter n)
   (if (= n 0)
@@ -36,4 +84,4 @@
 
 (define painter (make-painter 1))
 
-(up-split painter 4)
+(display (serialize-painting (paint (up-split painter 3))))
