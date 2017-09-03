28be4b47d7dcb30c81bde9ec2c6972e49597a65a
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-09-03 15:42:53 +0300

    E2.46-2.49.3 - Frames and vectors, first real painters
    I'm pretty certain about the correctness of the three implemented
    painters, but before I can start implementing the `wave` painter, I
    really need something that actually draws something onto to the screen,
    because otherwise it would be very hard to tell if my implementation is
    reasonable or not.

--- exercise-2.44-2.52.scm
+++ exercise-2.44-2.52.scm
@@ -122,4 +122,84 @@
 
 (define painter (make-painter 1))
 
-(display (serialize-painting (paint (square-limit painter 1))))
+; 2.46
+(define make-vect cons)
+(define xcor-vect car)
+(define ycor-vect cdr)
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2))
+             (+ (ycor-vect v1) (ycor-vect v2))))
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2))
+             (- (ycor-vect v1) (ycor-vect v2))))
+(define (scale-vect s v)
+  (make-vect (* s (xcor-vect v))
+             (* s (ycor-vect v))))
+
+; 2.47
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+(define origin-frame car)
+(define edge1-frame cadr)
+(define edge2-frame caddr)
+
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+(define origin-frame car)
+(define edge1-frame cadr)
+(define edge2-frame cddr)
+
+; 2.48
+(define make-segment cons)
+(define start-segment car)
+(define end-segment cdr)
+
+; From the book
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+      (origin-frame frame)
+      (add-vect
+        (scale-vect (xcor-vect v)
+                    (edge1-frame frame))
+        (scale-vect (ycor-vect v)
+                    (edge2-frame frame))))))
+
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+      (lambda (segment)
+        (draw-line
+          ((frame-coord-map frame)
+           (start-segment segment))
+          ((frame-coord-map frame)
+           (end-segment segment))))
+      segment-list)))
+
+; 2.49
+(define frame-outline-painter
+  (segments->painter (list (make-segment (make-vect 0 0)
+                                         (make-vect 0 1))
+                           (make-segment (make-vect 0 1)
+                                         (make-vect 1 1))
+                           (make-segment (make-vect 1 1)
+                                         (make-vect 1 0))
+                           (make-segment (make-vect 1 0)
+                                         (make-vect 0 0)))))
+
+(define x-painter
+  (segments->painter (list (make-segment (make-vect 0 0)
+                                         (make-vect 1 1))
+                           (make-segment (make-vect 0 1)
+                                         (make-vect 1 0)))))
+
+(define diamond-painter
+  (segments->painter (list (make-segment (make-vect 0 0.5)
+                                         (make-vect 0.5 1))
+                           (make-segment (make-vect 0.5 1)
+                                         (make-vect 1 0.5))
+                           (make-segment (make-vect 1 0.5)
+                                         (make-vect 0.5 0))
+                           (make-segment (make-vect 0.5 0)
+                                         (make-vect 0 0.5)))))
