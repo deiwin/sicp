f87bd047dca271a19841b6da0da8edbe9a3b58dd
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-09-10 13:53:22 +0300

    E2.60 - Set as ordered list with duplicates
    The complexity of `adjoin-set` changed from O(n) to O(1). The complexity of
    `union-set` changed from O(m*n) to O(n), where n is the size of the first set
    (actually it's underlying list's length) and m is the size of the second set.
    
    The complexity of `element-of-set?` and `intersection-set` ostensibly remained
    the same, while actually they get worse. Their big O notation complexity didn't
    change, but the value of n is generally going to be larger, because it includes
    duplicate values as well.
    
    This implementation is good in cases where insertions into the set need to be
    very fast and memory exhaustion will not be a problem. For example in
    situations where data collection into sets needs to be very fast, but the list
    can later be deduped. When there's no more time pressure.

--- exercise-2.59-2.60.scm
+++ exercise-2.59-2.60.scm
@@ -1,13 +1,9 @@
-; From the book
 (define (element-of-set? x set)
   (cond ((null? set) false)
         ((equal? x (car set)) true)
         (else (element-of-set? x (cdr set)))))
 
-(define (adjoin-set x set)
-  (if (element-of-set? x set)
-    set
-    (cons x set)))
+(define adjoin-set cons)
 
 (define (intersection-set set1 set2)
   (cond ((or (null? set1) (null? set2))
@@ -19,15 +15,11 @@
         (else (intersection-set (cdr set1)
                                 set2))))
 
-; 2.59
-(define (union-set set1 set2)
-  (cond ((null? set1) set2)
-        ((element-of-set? (car set1) set2)
-         (union-set (cdr set1) set2))
-        (else (cons (car set1)
-                    (union-set (cdr set1) set2)))))
+(define union-set append)
 
-(and (equal? (intersection-set '(1 2 3) '(2 3 4))
+(and (equal? (adjoin-set 3 '(1 2 3))
+             '(3 1 2 3))
+     (equal? (intersection-set '(1 2 3) '(2 3 4))
              '(2 3))
      (equal? (union-set '(1 2 3) '(2 3 4))
-             '(1 2 3 4)))
+             '(1 2 3 2 3 4)))
