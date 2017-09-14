b0286f0a2566d47960a7dd581c886bbaae880d64
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-09-14 18:51:39 +0300

    Exercise 1.8 Pascal Triangle

--- mob.scm
+++ mob.scm
@@ -79,9 +79,12 @@
 (define (sqrt x)
   (sqrt-iter 1.0 x 0))
 
-; Exercise 1.8: Newton’s method for cube roots is based on the fact that if y is an approximation to the cube root of x , then a better approximation is given by the value
-; x / y 2 + 2 y 3 .
-; Use this formula to implement a cube-root procedure analogous to the square-root procedure. (In 1.3.4 we will see how to implement Newton’s method in general as an abstraction of these square-root and cube-root procedures.)
+; Exercise 1.8: Newton’s method for cube roots is based on the fact that if y
+; is an approximation to the cube root of x , then a better approximation is
+; given by the value x / y 2 + 2 y 3 .  Use this formula to implement a
+; cube-root procedure analogous to the square-root procedure. (In 1.3.4 we will
+; see how to implement Newton’s method in general as an abstraction of these
+; square-root and cube-root procedures.)
 
 (define (cbrt x)
   (define (cbrt-iter guess x step)
@@ -148,3 +151,19 @@
 	 (= (f-iter 2) 2)
 	 (= (f-iter 3) 4)
 	 (= (f-iter 5) 25))
+
+; Exercise 1.12 Write a procedure that computes elements of
+; Pascal’s triangle by means of a recursive process.
+(define (pascal n)
+  (define (new-row old-row)
+    (if (not (null? (cdr old-row)))
+      (cons (+ (car old-row) (cadr old-row)) (new-row (cdr old-row)))
+      '(1)))
+  (if (= n 0)
+      '(1)
+      (cons 1 (new-row (pascal (- n 1))))))
+
+(and (equal? (pascal 0) '(1))
+     (equal? (pascal 1) '(1 1))
+     (equal? (pascal 2) '(1 2 1))
+     (equal? (pascal 3) '(1 3 3 1)))
