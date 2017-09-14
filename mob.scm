828534f721fa0e57c5716df839d1bf773686697c
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-09-14 17:52:10 +0300

    Add exercise 1.8

--- mob.scm
+++ mob.scm
@@ -78,3 +78,18 @@
   (< (abs (- (/ guess next-guess) 1)) 0.000001))
 (define (sqrt x)
   (sqrt-iter 1.0 x 0))
+
+; Exercise 1.8: Newton’s method for cube roots is based on the fact that if y is an approximation to the cube root of x , then a better approximation is given by the value
+; x / y 2 + 2 y 3 .
+; Use this formula to implement a cube-root procedure analogous to the square-root procedure. (In 1.3.4 we will see how to implement Newton’s method in general as an abstraction of these square-root and cube-root procedures.)
+
+(define (cbrt x)
+  (define (cbrt-iter guess x step)
+    (if (good-enough? guess (improve-by-newton guess x))
+        (values guess step)
+      (cbrt-iter (improve-by-newton guess x) x (+ step 1))))
+  (define (improve-by-newton guess x)
+    (/ (+ (* guess 2) (/ x (square guess))) 3))
+  (define (good-enough? guess next-guess)
+    (< (abs (- (/ guess next-guess) 1)) 0.000001))
+  (cbrt-iter 1.0 x 0))
