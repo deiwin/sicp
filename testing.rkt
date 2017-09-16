e159d06ae5ac08e8d37a3c6cf97e25c3efd5d702
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-09-16 21:36:54 +0300

    Rewrite assert macro to get rid of function dependencies
    With this version, the expanded code does not depend on functions such as
    `assert-and` and `assert-or`. The logic of those functions is embedded in the
    macro itself now.
    
    Also use syntax matching over the more manual logic implemented in the previous
    version.

---
+++ testing.rkt
@@ -0,0 +1,84 @@
+(define-syntax (assert stx)
+  (define (make-equal? a b)
+    #`(if (equal? #,a #,b)
+        #t
+        (let ([message
+                (string-append
+                  (pretty-format #,a)
+                  " does not equal? "
+                  (pretty-format #,b))])
+          (list 'failed (list message)))))
+  (define (make= a b)
+    #`(if (= #,a #,b)
+        #t
+        (let ([message
+                (string-append
+                  (pretty-format #,a)
+                  " does not = "
+                  (pretty-format #,b))])
+          (list 'failed (list message)))))
+  (define (make-combine-failure f1 f2)
+    #`(list 'failed (append (cadr #,f1)
+                            (cadr #,f2))))
+  (define (make-failed? assert-result-stx)
+    #`(and (pair? #,assert-result-stx)
+           (equal? (car #,assert-result-stx) 'failed)))
+  (define (make-and assert-stx-list)
+    #`(foldl
+        (lambda (assert-result acc)
+          (cond (#,(make-failed? #'assert-result) (if #,(make-failed? #'acc)
+                                                    #,(make-combine-failure #'assert-result #'acc)
+                                                    assert-result))
+                (assert-result acc)
+                (else
+                  (error "macro wrong, got #f instead of (list 'failed (list))"))))
+        #t
+        (list #,@assert-stx-list)))
+  (define (make-or assert-stx-list)
+    (define (make-or-combine head rest)
+      #`(cond [(not #,rest) #,head]
+              [#,(make-failed? rest) #,(make-combine-failure head rest)]
+              [else #t]))
+    #`(foldl
+        (lambda (assert-result acc)
+          (cond (#,(make-failed? #'assert-result) #,(make-or-combine #'assert-result #'acc))
+                (assert-result #t)
+                (else
+                  (error "macro wrong, got #f instead of (list 'failed (list))"))))
+        #f
+        (list #,@assert-stx-list)))
+  (define (make-assert bool-stx)
+    (syntax-case
+      bool-stx
+      (equal? = and)
+      [(equal? a b) (make-equal? #'a #'b)]
+      [(= a b) (make= #'a #'b)]
+      [(and bools ...) (make-and (map make-assert
+                                      (syntax->list #'(bools ...))))]
+      [(or bools ...) (make-or (map make-assert
+                                    (syntax->list #'(bools ...))))]))
+  (define (make-color-red assert-stx)
+    #`(string-append
+        "\033[31m"
+        (string-join (string-split #,assert-stx "\n")
+                     "\033[0m\n\033[31m")
+        "\033[0m"))
+  (syntax-case stx ()
+    [(assert bool-exp)
+     #`(let ([result #,(make-assert #'bool-exp)])
+         (if (equal? #t result)
+           #t
+           (error #,(make-color-red #'(string-append
+                                        "Assertion error!\n  * "
+                                        (string-join (cadr result)
+                                                     "\n  * "))))))]))
+
+
+(require macro-debugger/expand)
+(syntax->datum (expand-only #'(assert (or (equal? '(1) '(2))
+                                          (and (= 1 2)
+                                               (= 1 1))))
+                            (list #'assert)))
+(assert (or (equal? '(1) '(2))
+            (and (= 1 2)
+                 (= 1 1))))
