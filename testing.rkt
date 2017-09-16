a5e7efb7cc499009ecd62604823d60716f2c3773
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-09-16 22:14:28 +0300

    Remove unnecessary make- prefixes
    Where they remained, they're actually helpful for differentiating between the
    functions themselves and the functions they're wrapping.

--- testing.rkt
+++ testing.rkt
@@ -18,31 +18,31 @@
                              #:before-last " and "
                              #:after-last " are not =")])
           (list 'failed (list message)))))
-  (define (make-combine-failure f1 f2)
+  (define (combine-failure f1 f2)
     #`(list 'failed (append (cadr #,f1)
                             (cadr #,f2))))
-  (define (make-failed? assert-result-stx)
+  (define (failed? assert-result-stx)
     #`(and (pair? #,assert-result-stx)
            (equal? (car #,assert-result-stx) 'failed)))
   (define (make-and assert-stx-list)
     #`(foldl
         (lambda (assert-result acc)
-          (cond (#,(make-failed? #'assert-result) (if #,(make-failed? #'acc)
-                                                    #,(make-combine-failure #'assert-result #'acc)
-                                                    assert-result))
+          (cond (#,(failed? #'assert-result) (if #,(failed? #'acc)
+                                               #,(combine-failure #'assert-result #'acc)
+                                               assert-result))
                 (assert-result acc)
                 (else
                   (error "macro wrong, got #f instead of (list 'failed (list))"))))
         #t
         (list #,@assert-stx-list)))
   (define (make-or assert-stx-list)
-    (define (make-or-combine head rest)
+    (define (or-combine head rest)
       #`(cond [(not #,rest) #,head]
-              [#,(make-failed? rest) #,(make-combine-failure head rest)]
+              [#,(failed? rest) #,(combine-failure head rest)]
               [else #t]))
     #`(foldl
         (lambda (assert-result acc)
-          (cond (#,(make-failed? #'assert-result) #,(make-or-combine #'assert-result #'acc))
+          (cond (#,(failed? #'assert-result) #,(or-combine #'assert-result #'acc))
                 (assert-result #t)
                 (else
                   (error "macro wrong, got #f instead of (list 'failed (list))"))))
@@ -58,7 +58,7 @@
                                       (syntax->list #'(bools ...))))]
       [(or bools ...) (make-or (map make-assert
                                     (syntax->list #'(bools ...))))]))
-  (define (make-color-red assert-stx)
+  (define (color-red assert-stx)
     #`(string-append
         "\033[31m"
         (string-join (string-split #,assert-stx "\n")
@@ -69,10 +69,10 @@
      #`(let ([result #,(make-assert #'bool-exp)])
          (if (equal? #t result)
            #t
-           (error #,(make-color-red #'(string-append
-                                        "Assertion error!\n  * "
-                                        (string-join (cadr result)
-                                                     "\n  * "))))))]))
+           (error #,(color-red #'(string-append
+                                   "Assertion error!\n  * "
+                                   (string-join (cadr result)
+                                                "\n  * "))))))]))
 
 
 (require macro-debugger/expand)
