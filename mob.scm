832b45b453b658c411bad97353bc4c3ec2f6b1a1
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-09-19 18:25:56 +0300

    Exercise 1.16

--- mob.scm
+++ mob.scm
@@ -182,8 +182,6 @@
 (define (cube x) (* x x x))
 (define (p x) (- (* 3 x) (* 4 (cube x))))
 (define (sine angle)
-   (display angle)
-   (display "\n")
    (if (not (> (abs angle) 0.1))
        angle
        (p (sine (/ angle 3.0)))))
@@ -193,3 +191,33 @@
 ; O(log(a))
 
 (log 10000 3)
+
+; Exercise 1.16: Design a procedure that evolves an iterative exponentiation
+; process that uses successive squaring and uses a logarithmic number of steps,
+; as does fast-expt. (Hint: Using the observation that ( b n / 2 ) 2 = ( b 2 ) n / 2 ,)
+; keep, along with the exponent n and the base b , an additional state variable a ,
+; and define the state transformation in such a way that the product a b n is unchanged
+; from state to state. At the beginning of the process a is taken to be 1, and the
+; answer is given by the value of a at the end of the process. In general, the technique
+; of defining an invariant quantity that remains unchanged from state to state is a powerful
+; way to think about the design of iterative algorithms.
+
+(define (fast-expt b n)
+  (cond ((= n 0)
+         1)
+        ((even? n)
+         (square (fast-expt b (/ n 2))))
+        (else
+         (* b (fast-expt b (- n 1))))))
+
+(define (fast-expt-iter b n)
+  (define (iter b n a)
+    (cond ((= n 0) a)
+          ((even? n) (iter (square b) (/ n 2) a))
+          (else (iter b (- n 1) (* a b)))))
+  (iter b n 1))
+
+(assert "Is same as fast-expt"
+  (and (= (fast-expt 2 2) (fast-expt-iter 2 2))
+       (= (fast-expt 3 3) (fast-expt-iter 3 3))
+       (= (fast-expt 2 7) (fast-expt-iter 2 7))))
