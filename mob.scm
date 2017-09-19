39ddffb62251730610376cb0a462ab62cc019fe6
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-09-19 18:34:34 +0300

    Exercise 1.17

--- mob.scm
+++ mob.scm
@@ -221,3 +221,32 @@
   (and (= (fast-expt 2 2) (fast-expt-iter 2 2))
        (= (fast-expt 3 3) (fast-expt-iter 3 3))
        (= (fast-expt 2 7) (fast-expt-iter 2 7))))
+
+; Exercise 1.17: The exponentiation algorithms in this section are
+; based on performing exponentiation by means of repeated multiplication. In a
+; similar way, one can perform integer multiplication by means of repeated
+; addition. The following multiplication procedure (in which it is assumed that)
+; our language can only add, not multiply is analogous to the expt procedure:
+
+(define (* a b)
+  (if (= b 0)
+      0
+      (+ a (* a (- b 1)))))
+
+; This algorithm takes a number of steps that is linear in b. Now suppose we
+; include, together with addition, operations double, which doubles an integer,
+; and halve, which divides an (even) integer by 2. Using these, design a
+; multiplication procedure analogous to fast-expt that uses a logarithmic number
+; of steps.
+
+(define (double a) (+ a a))
+
+(define (fast-* a b)
+  (cond ((= b 0) 0)
+        ((even? b) (double (fast-* a (/ b 2))))
+        (else
+          (+ a (fast-* a (- b 1))))))
+
+(assert "It multiplies"
+  (and (= (fast-* 2 2) 4)
+       (= (fast-* 5 6) 30)))
