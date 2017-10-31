05122e2fce87b9c1f712ba9befd07de0d6695f5e
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-10-31 17:47:44 +0200

    E2.3 - Rectangles and their areas/perimeters

--- mob2.scm
+++ mob2.scm
@@ -30,3 +30,47 @@
              (equal? (make-point 1 1)
                      (midpoint-segment (make-segment (make-point 2 2)
                                                      (make-point 0 0))))))
+
+(define (square x) (* x x))
+(define (segment-length segment)
+  (let ((start-x (x-point (start-segment segment)))
+        (end-x (x-point (end-segment segment)))
+        (start-y (y-point (start-segment segment)))
+        (end-y (y-point (end-segment segment))))
+    (sqrt (+ (square (- start-x end-x))
+             (square (- start-y end-y))))))
+
+(assert "can calc segment length"
+        (and (= 10 (segment-length (make-segment (make-point 0 0)
+                                                 (make-point 0 10))))
+             (= (sqrt 2) (segment-length (make-segment (make-point 0 0)
+                                                       (make-point 1 1))))))
+
+(define (make-rectangle top-left bottom-left bottom-right)
+  (list top-left bottom-left bottom-right))
+(define (rectangle-left-side r)
+  (make-segment (cadr r) (car r)))
+(define (rectangle-bottom-side r)
+  (make-segment (cadr r) (caddr r)))
+
+(define (rectangle-height r)
+  (segment-length (rectangle-left-side r)))
+(define (rectangle-width r)
+  (segment-length (rectangle-bottom-side r)))
+
+(define (rectangle-area r)
+  (* (rectangle-width r) (rectangle-height r)))
+
+(define (rectangle-perimeter r)
+  (* 2 (+ (rectangle-width r)
+          (rectangle-height r))))
+
+(assert "can calc rectangle area"
+        (= 50 (rectangle-area (make-rectangle (make-point 0 5)
+                                              (make-point 0 0)
+                                              (make-point 10 0)))))
+
+(assert "can calc rectangle perimeter"
+        (= 30 (rectangle-perimeter (make-rectangle (make-point 0 5)
+                                                   (make-point 0 0)
+                                                   (make-point 10 0)))))
