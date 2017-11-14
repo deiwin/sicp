83a20e502f48341e7e80a67723e1adc93e5f8fae
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-11-14 17:26:30 +0200

    Exec-2.34.

--- mob2.scm
+++ mob2.scm
@@ -306,3 +306,14 @@
   (accumulate (lambda (x acc) (add1 acc))
               0 sequence))
 (assert (= 4 (my-length '(1 2 3 4))))
+
+
+(define
+  (horner-eval x coefficient-sequence)
+  (accumulate
+   (lambda (this-coeff higher-terms)
+     (+ (* higher-terms x) this-coeff))
+   0
+   coefficient-sequence))
+
+(assert (= 79 (horner-eval 2 '(1 3 0 5 0 1))))
