4d32eaf55a67d99c1faa9634afa223eba48aeb81
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-11-28 17:49:58 +0200

    Ex 2.62 - Union of ordered set list

--- mob2.scm
+++ mob2.scm
@@ -558,7 +558,13 @@
                           set1
                           (cdr set2)))))))
 (define (union-set set1 set2)
-  (foldl adjoin-set set2 set1))
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        ((< (car set1) (car set2)) (cons (car set1) (union-set (cdr set1) set2)))
+        ((= (car set1) (car set2)) (cons (car set1) (union-set (cdr set1) (cdr set2))))
+        (else (cons (car set2) (union-set set1 (cdr set2))))))
 
 (assert (and (equal? '(2) (intersection-set '(1 2) '(2 3)))
-             (equal? '(1 2 3) (union-set '(1 2) '(3)))))
+             (equal? '(1 2 3) (union-set '(1 2) '(3)))
+             (equal? '() (union-set '() '()))
+             (equal? '(1 2 3) (union-set '(1 3) '(1 2)))))
