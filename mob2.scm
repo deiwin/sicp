9466977042d2f479a840dd1a2dd99c1a2118ff8b
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-12-12 17:22:34 +0200

    E2.67 - Copy and run

--- mob2.scm
+++ mob2.scm
@@ -662,3 +662,65 @@
                (and (equal? tony (lookup (key tony) (list->tree (list tony))))
                     (equal? ostap (lookup (key ostap) (list->tree (list ostap tony urmas))))
                     (equal? urmas (lookup (key urmas) (list->tree (list ostap tony urmas))))))))
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left)
+                (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
+(define (symbols-branch tree) (caddr tree))
+(define (weight-branch tree) (cadddr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (symbols-branch tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (weight-branch tree)))
+
+(define (decode bits tree)
+  (define (choose-branch bit branch)
+    (cond ((= bit 0) (left-branch branch))
+          ((= bit 1) (right-branch branch))
+          (else (error "bad bit:
+                       CHOOSE-BRANCH" bit))))
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+      '()
+      (let ((next-branch
+              (choose-branch
+                (car bits)
+                current-branch)))
+        (if (leaf? next-branch)
+          (cons
+            (symbol-leaf next-branch)
+            (decode-1 (cdr bits) tree))
+          (decode-1 (cdr bits)
+                    next-branch)))))
+  (decode-1 bits tree))
+
+
+(assert "can decode"
+        (let ((sample-tree (make-code-tree
+                             (make-leaf 'A 4)
+                             (make-code-tree
+                               (make-leaf 'B 2)
+                               (make-code-tree
+                                 (make-leaf 'D 1)
+                                 (make-leaf 'C 1)))))
+              (sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0)))
+          (equal? '(A D A B B C A) (decode sample-message sample-tree))))
