526397c5983e7b8c5de136c7a3adbcc00f3532ed
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-12-12 18:29:06 +0200

    Ex 2.70
    Everybody knows it is 84

--- mob2.scm
+++ mob2.scm
@@ -775,3 +775,19 @@
 
 (assert "generate-huffman-tree"
         (equal? sample-tree (generate-huffman-tree '((A 4) (B 2) (C 1) (D 1)))))
+
+(define song-tree (generate-huffman-tree
+                    '((BOOM 1) (WAH 1) (A 2) (GET 2) (JOB 2) (SHA 3) (YIP 9) (NA 16))))
+(define song-lyrics
+  '(GET A JOB
+    SHA NA NA NA NA NA NA NA NA
+
+    GET A JOB
+    SHA NA NA NA NA NA NA NA NA
+
+    WAH YIP YIP YIP YIP
+    YIP YIP YIP YIP YIP
+    SHA BOOM))
+(define get-a-job (encode song-lyrics song-tree))
+
+(assert (= 84 (length get-a-job)))
