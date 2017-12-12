e5c1d72e3ab2f7350c40066651088995846a91dd
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-12-12 18:18:43 +0200

    E2.69 - Generate Huffman trees

--- mob2.scm
+++ mob2.scm
@@ -744,3 +744,34 @@
 
 (assert "encodes message"
         (equal? sample-message (encode '(A D A B B C A) sample-tree)))
+
+(define (generate-huffman-tree pairs)
+  (define (adjoin-set x set)
+    (cond ((null? set) (list x))
+          ((< (weight x) (weight (car set)))
+           (cons x set))
+          (else
+            (cons (car set)
+                  (adjoin-set x (cdr set))))))
+
+  (define (make-leaf-set pairs)
+    (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set
+          (make-leaf (car pair)    ; symbol
+                     (cadr pair))  ; frequency
+          (make-leaf-set (cdr pairs))))))
+
+  (define (successive-merge tree-set)
+    (cond ((null? (cdr tree-set)) (car tree-set))
+          (else (successive-merge
+                  (adjoin-set
+                    (make-code-tree (car tree-set) (cadr tree-set))
+                    (cddr tree-set))))))
+
+  (successive-merge
+    (make-leaf-set pairs)))
+
+(assert "generate-huffman-tree"
+        (equal? sample-tree (generate-huffman-tree '((A 4) (B 2) (C 1) (D 1)))))
