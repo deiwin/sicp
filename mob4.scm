444bcdd2e49cfcdf9d840c3e0a36d69dc306bb58
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2018-03-27 18:02:57 +0300

    Add make-account

--- mob4.scm
+++ mob4.scm
@@ -35,3 +35,20 @@
           (and (= sqrt-of-hundred 10)
                (= call-count 1)
                (= call-count2 0))))
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance
+                     (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request:
+                 MAKE-ACCOUNT" m))))
+  dispatch)
