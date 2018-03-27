203072bb1fc81d81152bb14ba53e4b610fc95606
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2018-03-27 18:00:03 +0300

    Ex 3.2

--- mob4.scm
+++ mob4.scm
@@ -14,3 +14,24 @@
           (and (= a1 6)
                (= a2 8)
                (= a3 11))))
+
+(define (make-monitored f)
+  (let ((call-count 0))
+    (lambda (x)
+      (cond ((eq? x 'how-many-calls?) call-count)
+            ((eq? x 'reset-count) (set! call-count 0))
+            (else
+              (set! call-count (add1 call-count))
+              (f x))))))
+
+(let* ((s (make-monitored sqrt))
+       (sqrt-of-hundred (s 100))
+       (call-count (s 'how-many-calls?))
+       (call-count2
+         (begin
+           (s 'reset-count)
+           (s 'how-many-calls?))))
+  (assert "can monitor"
+          (and (= sqrt-of-hundred 10)
+               (= call-count 1)
+               (= call-count2 0))))
