2ace3dfc982bbed6d6b1a761c0d4aa13e41dc77d
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2018-03-27 18:47:55 +0300

    Ex 3.4 - Reset failed count after successful attempt

--- mob4.scm
+++ mob4.scm
@@ -37,6 +37,8 @@
                (= call-count2 0))))
 
 (define (make-account balance password)
+  (define (call-the-cops) (error "Cops are coming!"))
+  (define failed-attempts 0)
   (define (withdraw amount)
     (if (>= balance amount)
         (begin (set! balance
@@ -47,17 +49,41 @@
     (set! balance (+ balance amount))
     balance)
   (define (dispatch provided-password m)
-    (cond ((not (eq? provided-password password)) (error "Incorrect password"))
-          ((eq? m 'withdraw) withdraw)
-          ((eq? m 'deposit) deposit)
-          (else (error "Unknown request:
-                 MAKE-ACCOUNT" m))))
+    (if (not (eq? provided-password password))
+        (begin
+         (set! failed-attempts (add1 failed-attempts))
+         (if (> failed-attempts 7)
+           (call-the-cops)
+           (error "Incorrect password")))
+        (begin
+         (set! failed-attempts 0)
+         (cond
+           ((eq? m 'withdraw) withdraw)
+           ((eq? m 'deposit) deposit)
+           (else (error "Unknown request:
+                  MAKE-ACCOUNT" m))))))
   dispatch)
 
 (let* ((acc (make-account 100 'secret-password))
        (balance-after-withdrawal ((acc 'secret-password 'withdraw) 40))
        (invalid-password-withdrawal
-        (with-handlers ((exn:fail? (lambda (exn) (exn-message exn))))
-          ((acc 'invalid-password 'withdraw) 40))))
+         (with-handlers ((exn:fail? exn-message))
+                        ((acc 'invalid-password 'withdraw) 40)))
+       (too-many-invalid-withdrawals
+         (begin
+           (with-handlers ((exn:fail? exn-message)) ((acc 'x 'withdraw) 40))
+           (with-handlers ((exn:fail? exn-message)) ((acc 'x 'withdraw) 40))
+           (with-handlers ((exn:fail? exn-message)) ((acc 'x 'withdraw) 40))
+           (with-handlers ((exn:fail? exn-message)) ((acc 'x 'withdraw) 40))
+           (with-handlers ((exn:fail? exn-message)) ((acc 'x 'withdraw) 40))
+           (with-handlers ((exn:fail? exn-message)) ((acc 'x 'withdraw) 40))
+           (with-handlers ((exn:fail? exn-message)) ((acc 'x 'withdraw) 40))))
+       (unsuccessful-login-attempt-after-successful-login-attempt-after-cops
+         (begin
+           ((acc 'secret-password 'withdraw) 40)
+           (with-handlers ((exn:fail? exn-message)) ((acc 'x 'withdraw) 40)))))
+
   (assert (and (= 60 balance-after-withdrawal)
-               (equal? "Incorrect password" invalid-password-withdrawal))))
+               (equal? "Incorrect password" invalid-password-withdrawal)
+               (equal? "Cops are coming!" too-many-invalid-withdrawals)
+               (equal? "Incorrect password" unsuccessful-login-attempt-after-successful-login-attempt-after-cops))))
